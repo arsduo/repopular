1d751f3b451236cf0a7211c92778703aa71b3ec6
Alex Koppel <alex+git@alexkoppel.com>
2012-09-11 14:16:23 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Repopular::Application.config.secret_token = 'c6eceb952fe174473fb02b932841ccf183403931985eea089564666fc4e7059e6a7421d7b5ea527ad569e5253e208b7b88e46bdb0d7a04512cb83158f6ef9b88'
