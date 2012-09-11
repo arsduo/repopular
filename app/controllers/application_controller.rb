ee8fdda56b4651dc87ae9c949b41a98d98b8b426
Alex Koppel <alex+git@alexkoppel.com>
2012-09-11 18:16:33 +0200

    Update user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  helper_method :user
+  def user
+    session[:user]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   attr_accessible :fb_id, :fb_token, :gh_id, :gh_token, :name
+
+  class GitTokenMissing < StandardError; end
+  def git_repos
+    raise GitTokenMissing unless self.gh_token
+    GH.with(token: gh_token) { GH["user/repos"] }
+  end
+
+  def facebook?
+    !!fb_token
+  end
+
+  def github?
+    !!gh_token
+  end
+
+  def authenticated?
+    gh_token && fb_token
+  end
 end
