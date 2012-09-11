1d2b1d9b9448097903070866b0fc6eccc4100c97
Isaac Wolkerstorfer <i@agnoster.net>
2012-09-11 16:01:20 +0200

    Add function to create hook for repo

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,3 +1,17 @@
 class Repo < ActiveRecord::Base
   attr_accessible :full_name, :name, :user_id
+
+  # gh - an authenticated client
+  # events - Array of events to subscribe to
+  def create_hook(gh, *events)
+    gh.post path("hooks"),
+      name: "web",
+      active: true,
+      config: { url: ENV['WEBHOOK_URL'] },
+      events: events.map(&:to_s)
+  end
+
+  def path(*subpath)
+    (["repos", full_name] + subpath).join("/")
+  end
 end
